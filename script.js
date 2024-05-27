244deb69d37c1f71554f848dadb8d6a3b8acb408
Andrea Puebla <107224227+andreagpuebla@users.noreply.github.com>
2024-05-26 22:57:28 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+/* vinculado a paginas index y contacto*/
+
+alert("Bienvenido a Mística")
+
+var ciudad = prompt ("Ingresa tu ciudad");
+
+switch (ciudad.toLocaleLowerCase()) {
+    case "san nicolas":
+    case " villa constitucion":
+    case "conesa":
+    case "ramallo":
+        alert ("Tenes envío gratis")
+        break;
+    default:
+        alert ("El costo de envío es $800");
+        break;
+}
+
+/* Boton Whatsapp*/
+
+document.getElementById('whatsappButton').addEventListener('click', function() {
+    const phoneNumber = '543412732531'; 
+    const message = 'Hola Tienda Mistica!, tengo una consulta,'; 
+    const whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
+    
+    window.open(whatsappUrl, '_blank');
+}); 
+
+window.addEventListener('beforeunload', function(event) {
+    const message = '¿Estás seguro de que quieres salir?';
+    event.preventDefault();  
+    event.returnValue = message; 
+    return message;
+});
+
